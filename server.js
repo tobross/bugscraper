263c9acdeaded4fa4ffbb18edb8a216a175ffd4e
Tobey Ross <tobross@gmail.com>
2018-06-13 15:44:37 -0500

    completed server.js

--- server.js
+++ server.js
@@ -1,89 +1,59 @@
-// Dependencies
-var express = require("express");
-var mongojs = require("mongojs");
-// Require request and cheerio. This makes the scraping possible
-var request = require("request");
-var cheerio = require("cheerio");
-
-// Initialize Express
-var app = express();
-
-app.use(express.static("public"));
-var databaseUrl = "scraper";
-var collections = ["infoz"];
-
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
-
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
-
-app.get("/all", function(req, res) {
-  db.infoz.find({}, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/title", function(req, res) {
-  db.infoz.find().sort({ title: 1 }, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/points", function(req, res) {
-  db.infoz.find().sort({ points: -1 }, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/scrape", function(req, res) {
-  request("https://www.trueachievements.com/game/"+"Human-Fall-Flat"+"/achievements", function(error, response, html) {
-    var $ = cheerio.load(html);
-    $(".itemright").each(function(i, element) {
-      var title = $(element).children(".header").children(".mainlink").text();
-      var doIt = $(element).children(".subheader").children("span").text();
-      var points = $(element).children(".header").children("span").text();
-      if (title && doIt) {
-        db.infoz.insert({
-          title: title,
-          doIt: doIt,
-          points: points
-        },
-        function(err, inserted) {
-          if (err) {
-            console.log(err);
-          }
-          else {
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  res.send("Scrape Complete");
-});
+//dependencies
+const express = require('express'),
+      mongoose = require('mongoose'),
+      exphbs = require('express-handlebars'),
+      bodyParser = require('body-parser'),
+      logger = require('morgan'),
+      path = require('path'),
+      favicon = require('serve-favicon');
 
 
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+//initializing the app
+const app = express();
+
+//setting up the database
+const config = require('./config/database');
+mongoose.Promise = Promise;
+mongoose
+  .connect(config.database)
+  .then( result => {
+    console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+  })
+  .catch(err => console.log('There was an error with your connection:', err));
+
+//setting up favicon middleware
+app.use(favicon(path.join(__dirname, 'public', 'assets/img/favicon.ico')));
+
+//setting up Morgan middleware
+app.use(logger('dev'));
+
+//setting up body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//setting up handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//setting up the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/articles',express.static(path.join(__dirname, 'public')));
+app.use('/notes',express.static(path.join(__dirname, 'public')));
+
+
+//setting up routes
+const index = require('./routes/index'),
+      articles = require('./routes/articles'),
+      notes = require('./routes/notes'),
+      scrape = require('./routes/scrape');
+
+app.use('/', index);
+app.use('/articles', articles);
+app.use('/notes', notes);
+app.use('/scrape', scrape);
+
+//starting server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
